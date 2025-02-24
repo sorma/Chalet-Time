2a399ccbb0e5b2307410baf03542d9a2c97b5bdc
sorma <francesco.sorma03@gmail.com>
2025-02-24 23:39:37 +0100

    Modifica file build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.francesco.chalettime"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.francesco.chalettime"
